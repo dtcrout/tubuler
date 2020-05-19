1335575615393312eb03ffe8e2053edca2555c70
Darshan Crout <dtcrout@gmail.com>
2020-05-19 11:39:32 -0400

    Reformat src/main.js

--- src/main.js
+++ src/main.js
@@ -36,29 +36,29 @@ ipcMain.on('url', (event, url) => {
         console.log('No URL provided...');
         event.sender.send('log', '🔗 Please enter URL!');
     } else {
-        dialog.showOpenDialog(BrowserWindow.getFocusedWindow(), {properties: ['openDirectory', 'createDirectory']}).then(
-            (result) => {
-                if (result.canceled) {
-                    console.log('Dialog cancelled...');
-                } else {
-                    console.log('Downloading video...')
-                    event.sender.send('log', '⏳ Downloading video...')
+        const currentWindow = BrowserWindow.getFocusedWindow();
+        const dialogProperties = ['openDirectory', 'createDirectory'];
 
-                    const filePath = `${result.filePaths[0]}/%(title)s.%(ext)s`
+        dialog.showOpenDialog(currentWindow, {properties: dialogProperties}).then((result) => {
+            if (result.canceled) {
+                console.log('Dialog cancelled...');
+            } else {
+                console.log('Downloading video...');
+                event.sender.send('log', '⏳ Downloading video...');
 
-                    const payload = JSON.stringify(
-                        {
-                            "url": url,
-                            "ydl_opts": {
-                                "outtmpl": filePath
-                            }
-                        });
+                const filePath = `${result.filePaths[0]}/%(title)s.%(ext)s`;
 
-                    downloadVideo(payload).then(
-                        (result) => {
-                            event.sender.send('log', handleResponse(result))
-                        });
-                }
-            });
+                const payload = JSON.stringify({
+                    "url": url,
+                    "ydl_opts": {
+                        "outtmpl": filePath
+                    }
+                });
+
+                downloadVideo(payload).then((response) => {
+                    event.sender.send('log', handleResponse(response));
+                });
+            }
+        });
     }
 });
